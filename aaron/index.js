36a304ecd6e2909ccd4ed4508dfff01849be3932
Aaron Putterman <abp6318@rit.edu>
2020-06-03 15:57:44 -0400

    Trying to manage my gitignore file

--- aaron/index.js
+++ aaron/index.js
@@ -1,5 +1,5 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const Discord = require('discord.js')
 const config = require('./config')
