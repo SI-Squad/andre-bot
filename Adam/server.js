37e263abe0dc3dfa94b5292d27be40e652999297
Adam Densman <ard7246@g.rit.edu>
2020-06-02 21:35:52 -0400

    bot now dm's user message as well as sends translated message directly to server

--- Adam/server.js
+++ Adam/server.js
@@ -1,29 +1,3 @@
-// this is the way to load a package with NodeJS
-var express = require( "express" );
-
-// create an express object: a Web server
-var app = express();
-
-// When our server receives a certain request, it needs to know what it should respond,
-// so we are defining "routes" the server follows to return the right data.
-// When the server gets a request for the "root" route of this domain: "/"
-// here: "https://hello-node-server.glitch.me/"
-app.get( "/", function( request, response ) {
-  // we program the server to respond with an HTML string
-  response.send( "<h1>Hello :)</h1><p><a href='/about'>About</a></p>" ); 
-} );
-
-// when the server gets a request for "https://hello-node-server.glitch.me/about"
-app.get( "/about", function( request, response ) {
-  response.send( "<h1>This is my about page</h1><a href='/'>Home</a></p>" );
-} );
-
-// finally we tell the server to listen for requests
-app.listen( process.env.PORT );
-// the server needs to listen for requests on a specified port:
-// a port can be thought of as an access on device to communicate data
-// and computers have thousands of ports
-// on Glitch we can use "process.env.PORT" which corresponds to port 3000
 const Discord = require('discord.js')
 const client = new Discord.Client()
 client.on('message', msg => {
@@ -43,8 +17,9 @@ client.on('message', msg => {
     }
   }).then(res => {
     if (res.data.text[0] !== msg.content) {
-      msg.reply(res.data.text[0])
+      msg.author.send(res.data.text[0])
+      const channel = msg.channel
+      channel.send(res.data.text[0]);
     }
   })
 })
-
