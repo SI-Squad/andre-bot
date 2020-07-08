26c6e4650394a49b1b408b70abdb9c4fdc4f0b29
Aaron Putterman <abp6318@rit.edu>
2020-07-08 18:19:12 -0400

    Made it so you can add multiple people to DM lists and multiple channel lists that will now receive the transcription.

--- index.js
+++ index.js
@@ -207,26 +207,23 @@ discordClient.on('message', async msg => {
 		console.log(args[1])
 		console.log(parseID(args[1]))
 		break;
+	
+	// to be removed, used for testing
+	case 'test':
+		console.log(msg.member)
+		break;
 	}
 
 
 	if (msg.content === 'inHere') {
 		const memberVoiceChannel = msg.member.voice.channel
-		// console.log(memberVoiceChannel)
 
 		if (memberVoiceChannel == null) {
 			msg.reply("you need to be in a voice channel for me to help you!")
 			return
 		}
-		if(transChannel == null){
-			msg.reply("you need to type 'transcribeHere' in a text channel before I can start transcribing your message.")
-			return
-		}
 
 		msg.reply('I\'m omw')
-
-		// console.log("\n\n\n 2 \n\n\n")
-
 
 		const connection = await memberVoiceChannel.join()
 		const receiver = connection.receiver
@@ -237,9 +234,6 @@ discordClient.on('message', async msg => {
 			return
 			}
 
-			console.log("\n\n\n 3 \n\n\n")
-
-
 			console.log(`I'm listening to ${user.username}`)
 			const audioStream = receiver.createStream(user, { mode: 'pcm' })
 			const requestConfig = {
@@ -248,7 +242,6 @@ discordClient.on('message', async msg => {
 			languageCode: 'en-US'
 			}
 
-			// console.log("\n\n\n 4 \n\n\n")
 
 
 			const request = {
@@ -258,18 +251,23 @@ discordClient.on('message', async msg => {
 			.streamingRecognize(request)
 			.on('error', console.error)
 			.on('data', response => {
-				// console.log("\n\n\n reeeeeee \n\n\n")
 
 				const transcription = response.results
 				.map(result => result.alternatives[0].transcript)
 				.join('\n')
 				.toLowerCase()
 				console.log(`Transcription: ${transcription}`)
-				console.log(transChannel.id)
+				
 
-			
-				transChannel.send(`${user.username}: ${transcription}`)
-				// dmList.send(`${user.username}: ${transcription}`)
+				for(index=0; index<transChannelList.toArray().length; index++){
+					chan = msg.guild.channels.get(transChannelList.toArray()[index])
+					chan.send(`${user.username}: ${transcription}`)
+				}
+
+				for(i=0; i<transDMList.toArray().length; i++){
+					mem = msg.guild.members.fetch(transDMList.toArray()[i])
+					mem.then(function(member){member.send(`${user.username}: ${transcription}`)})
+				}
 
 
 			})
